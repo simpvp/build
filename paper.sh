31bc91fa031c4749ecda48a30c5c230bccfed012
Simpvp admins <github-admins@simplicitypvp.net>
2021-04-21 23:55:20 +0000

    update

--- paper.sh
+++ paper.sh
@@ -1,1 +1,1 @@
-exec java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -Xmx20G -jar paper-1.16.5-465.jar
+exec java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -Xmx20G -jar paper-1.16.5-592.jar
