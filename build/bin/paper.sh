51ded95a7ca35583b989dd60942d4fdb79321101
Simpvp admins <github-admins@simplicitypvp.net>
2022-07-23 09:40:41 +0000

    Update

--- build/bin/paper.sh
+++ build/bin/paper.sh
@@ -10,5 +10,5 @@ fi
 
 exec java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true \
 	-Xmx20G \
-	-jar ./bin/paper-1.17.1-408.jar
+	-jar ./bin/paper-1.18.2-386.jar
 
